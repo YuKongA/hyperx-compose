db215931d28b87ff220fbd5812376f2f24175b73
howie <howie.c@outlook.com>
2024-12-13 13:56:06 +0800

    misc: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ android {
 dependencies {
     api("top.yukonga.miuix.kmp:miuix:0.3.2")
     api("dev.chrisbanes.haze:haze:1.1.0")
-    api("androidx.compose.foundation:foundation:1.7.5")
+    api("androidx.compose.foundation:foundation:1.7.6")
     api("androidx.activity:activity-compose:1.9.3")
-    api("androidx.navigation:navigation-compose:2.8.4")
+    api("androidx.navigation:navigation-compose:2.8.5")
 }
