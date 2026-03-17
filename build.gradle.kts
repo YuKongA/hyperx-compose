132b9b4c05dbee082f2eb618f42f64dca951b3f3
howie <howie.c@outlook.com>
2026-03-17 13:42:14 +0800

    misc: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,11 +41,11 @@ android {
 @Suppress("UseTomlInstead")
 dependencies {
     api("top.yukonga.miuix.kmp:miuix:0.5.2")
-    api("dev.chrisbanes.haze:haze:1.7.1")
-    api("androidx.compose.foundation:foundation:1.10.2")
-    api("androidx.activity:activity-compose:1.12.3")
+    api("dev.chrisbanes.haze:haze:1.7.2")
+    api("androidx.compose.foundation:foundation:1.10.5")
+    api("androidx.activity:activity-compose:1.13.0")
     api("androidx.navigation:navigation-compose:2.9.7")
     api("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("com.github.promeg:tinypinyin:2.0.3") // maven("https://maven.aliyun.com/repository/public")
-    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.4.0")
 }
