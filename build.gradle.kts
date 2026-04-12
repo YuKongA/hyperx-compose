76e7c16beddeafbe328c45794ee2a9194c22631f
howie <howie.c@outlook.com>
2026-04-12 23:25:24 +0800

    chore: Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     api("androidx.compose.foundation:foundation:1.10.6")
     api("androidx.activity:activity-compose:1.13.0")
     api("top.yukonga.miuix.kmp:miuix-navigation3-ui:0.9.0")
-    api("androidx.navigation3:navigation3-runtime:1.1.0-rc01")
+    api("androidx.navigation3:navigation3-runtime:1.1.0")
     api("org.jetbrains.androidx.navigationevent:navigationevent-compose:1.0.1")
     api("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("com.github.promeg:tinypinyin:2.0.3") // maven("https://maven.aliyun.com/repository/public")
